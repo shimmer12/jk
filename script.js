93db68fd2672c168de25126882b3aa45bef4a78b
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-08-29 18:50:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Currently, no JavaScript functionality is needed for this static message
+// But you can add interactivity here if required in the future
+console.log("Page loaded: YOU HAVE BEEN FOOLED BHAIYA");
